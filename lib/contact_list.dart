6878cce817762a73a2c802147e6bd6d7e0d50788
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-01-17 01:39:45 +0100

    implemented dummy ontap and updated sdk constraints

--- lib/contact_list.dart
+++ lib/contact_list.dart
@@ -3,22 +3,26 @@ import 'package:flutter/material.dart';
 class ContactList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: getListView(context));
+    return Container(child: getListView(context));
   }
 
   Widget getListView(BuildContext context) {
     var data = getListItems();
 
-    return ListView.builder(itemBuilder: (context, index) {
-      return ListTile(title: Text("list-tile-title"),
-      subtitle: Text(data[index]));
-    },);
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return ListTile(
+            title: Text("list-tile-title"),
+            subtitle: Text(data[index]),
+          trailing: Icon(Icons.account_circle),
+          onTap: ()=>{debugPrint("${data[index]} tapped")},
+        );
+      },
+    );
   }
 
   List<String> getListItems() {
     var items = List<String>.generate(100, (index) => "This is item no $index");
     return items;
   }
-
 }
