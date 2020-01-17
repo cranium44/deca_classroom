9086722a0652b7198f0d25ab5318f66685b2f2ae
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-01-17 01:50:08 +0100

    added the floating action button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'home.dart';
 import 'contact_list.dart';
@@ -7,13 +8,19 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "My app",
       home: Scaffold(
         appBar: AppBar(title: Text("Landing Page"), elevation: 5.0),
         body: ContactList(),
+        floatingActionButton: FloatingActionButton(
+            onPressed: null,
+            tooltip: "Add new contact",
+            elevation: 4,
+            child: Icon(Icons.add),
+            foregroundColor: Color.fromRGBO(255, 255, 255, 1.0),
+            backgroundColor: Colors.redAccent),
       ),
     );
   }
